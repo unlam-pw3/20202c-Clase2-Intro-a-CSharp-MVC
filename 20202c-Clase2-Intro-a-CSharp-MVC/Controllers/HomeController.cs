c748bb4d2bc449b7386d8e02056d584f7f2f89b5
psanchez <psanchez@plus54.com>
2020-09-09 20:04:54 -0300

    agregando homecontroller y vistas vacias

---
+++ 20202c-Clase2-Intro-a-CSharp-MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20202c_Clase2_Intro_a_CSharp_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DiasDeLaSemana()
+        {
+            //Enunciado
+            //1)Crear una enumeración con los días de la semana,
+            //comenzando por Domingo con valor 
+            //1.2)Agregar a la enumeración la posibilidad de Imprimir un Texto por cada día de la semana
+
+            return View();
+        }
+    }
+}
---
+++ 20202c-Clase2-Intro-a-CSharp-MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_20202c_Clase2_Intro_a_CSharp_MVC.MvcApplication" Language="C#" %>
