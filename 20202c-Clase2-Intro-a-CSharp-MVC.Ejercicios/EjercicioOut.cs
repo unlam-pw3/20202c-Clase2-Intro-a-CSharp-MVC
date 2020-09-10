d39e5ab28971677b2d53960380d89ccd3939259c
psanchez <psanchez@plus54.com>
2020-09-09 21:37:11 -0300

    invirtiendo texto, aun sin out

---
+++ 20202c-Clase2-Intro-a-CSharp-MVC.Ejercicios/EjercicioOut.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20202c_Clase2_Intro_a_CSharp_MVC.Ejercicios
+{
+    public class EjercicioOut
+    {
+        public static string InvertirTexto(string texto)
+        {
+            char[] charArray = texto.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+
+    public static class ExtensionesString
+    {
+        public static string Cortar(this string text, int cantidad)
+        {
+            //extension methods
+
+            return text.Substring(0, cantidad);
+        }
+
+    }
+}
