7d3d248f50ff269b272d19577992f183d550871a
psanchez <psanchez@plus54.com>
2020-09-09 21:37:11 -0300

    invirtiendo texto, aun sin out

--- 20202c-Clase2-Intro-a-CSharp-MVC/Controllers/HomeController.cs
+++ 20202c-Clase2-Intro-a-CSharp-MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ejercicios = _20202c_Clase2_Intro_a_CSharp_MVC.Ejercicios;
 
 namespace _20202c_Clase2_Intro_a_CSharp_MVC.Controllers
 {
@@ -26,6 +27,22 @@ namespace _20202c_Clase2_Intro_a_CSharp_MVC.Controllers
             int diaDeLaSemana = ran.Next(1, 8);
             string nombreDia = EjercicioEnumeraciones.ObtenerNombreDiaDeLaSemana((EjercicioEnumeraciones.DiasDeLaSemana)diaDeLaSemana);
             ViewBag.NombreDia = nombreDia;
+
+            return View();
+        }
+
+        public ActionResult EjercicioOut()
+        {
+            //string textoOriginal = "Bella Y Bestia";
+            //string textoInvertido = Ejercicios.EjercicioOut.InvertirTexto(textoOriginal);
+
+            string textoOriginal = "Bella Y Bestia";
+            string textoInvertido;
+            Ejercicios.EjercicioOut.InvertirTexto(textoOriginal, out textoInvertido);
+
+            ViewBag.TextoOriginal = textoOriginal;
+            ViewBag.TextoInvertido = textoInvertido;
+
             return View();
         }
     }
